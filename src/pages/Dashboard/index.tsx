f27856e3ee863e26029218941ee74e3c29c5ec35
arllainm <arllain@gmail.com>
2020-06-15 05:31:50 -0300

    Criando páginas e rotas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { Button } from 'react-native';
 import { useAuth } from '../../hooks/auth';
+import { Container, Header, HeaderTitle, UserName } from './styles';
 
 const Dashboard: React.FC = () => {
   const { signOut } = useAuth();
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} color="#312e38" />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>Arllain Silva</UserName>
+        </HeaderTitle>
+      </Header>
+    </Container>
   );
 };
 
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import CreteAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
 
@@ -12,6 +15,10 @@ const AppRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreteAppointment" component={CreteAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
