8708850f9866c58c4400a35f1046a5bea99e63a2
arllainm <arllain@gmail.com>
2020-06-15 07:42:42 -0300

    Header do Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Button } from 'react-native';
 import { useAuth } from '../../hooks/auth';
-import { Container, Header, HeaderTitle, UserName } from './styles';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
+
   return (
     <Container>
       <Header>
         <HeaderTitle>
           Bem vindo, {'\n'}
-          <UserName>Arllain Silva</UserName>
+          <UserName>{user.name}</UserName>
         </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
       </Header>
+      {/* <Button title="Sair" onPress={signOut} color="#312e38" /> */}
     </Container>
   );
 };
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -6,6 +7,7 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
   background: #28262e;
   flex-direction: row;
   justify-content: space-between;
@@ -22,4 +24,12 @@ export const HeaderTitle = styled.Text`
 export const UserName = styled.Text`
   color: #ff9000;
   font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
 `;
